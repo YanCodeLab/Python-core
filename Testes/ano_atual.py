410ca38c83f3517e11f18737299c69834fe98155
Yan Rangel <157432600+YanCodeLab@users.noreply.github.com>
2026-09-21 16:26:44 -0300

    teste.data

--- Testes/ano_atual.py
+++ Testes/ano_atual.py
@@ -5,6 +5,10 @@ import datetime
 ano = date.today().year
 mes = date.today().month
 dia = date.today().day
-'''
+
 agora = datetime.datetime.now()
 print(agora.strftime("%d/%m/%Y %H:%M:%S")) # Reorganiza a posição da data e exibe horas 
+'''
+
+ano = datetime.date.today().year
+print(ano)
